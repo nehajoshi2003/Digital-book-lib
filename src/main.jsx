accaf2ed0c6821beba5053538cabe687c07d68fc
Neha Joshi <nehajoshi@Nehas-Laptop.local>
2025-07-16 21:16:32 +0530

    again2

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css';
-
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Digital-book-lib/">  {/* ✅ this fixes the routing */}
     <App />
   </BrowserRouter>
 );
